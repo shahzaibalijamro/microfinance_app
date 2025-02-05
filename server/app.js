37f611faad9f86ec551eb05d6edd5ba1ff63bc23
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2025-02-05 19:19:13 +0500

    frontend url added

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 const corsConfig = {
-    origin: 'http://localhost:3000',
+    origin: 'https://saylani-microfinance-app-by-shahzaib.vercel.app',
     credentials: true,
     methods: ["GET","POST","PUT","DELETE","PATCH"]
 }
